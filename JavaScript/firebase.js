ba9c447c0ff67422f062695813efe2a7ac5059ad
Keshav <keshav9899828464@gmail.com>
2026-01-24 14:08:55 +0530

    Update firebase.js

--- JavaScript/firebase.js
+++ JavaScript/firebase.js
@@ -1,12 +1,13 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAwl22tT7kJD1U0toxE0WckzOxHAao9Nzg",
-    authDomain: "emergency-healthcare-17c29.firebaseapp.com",
-    projectId: "emergency-healthcare-17c29",
-    storageBucket: "emergency-healthcare-17c29.firebasestorage.app",
-    messagingSenderId: "210921508011",
-    appId: "1:210921508011:web:8308e17f37863a40727bee"
-  };
+  apiKey: "AIzaSyAwl22tT7kJD1U0toxE0WckzOxHAao9Nzg",
+  authDomain: "emergency-healthcare-17c29.firebaseapp.com",
+  projectId: "emergency-healthcare-17c29",
+  storageBucket: "emergency-healthcare-17c29.firebasestorage.app",
+  messagingSenderId: "210921508011",
+  appId: "1:210921508011:web:8308e17f37863a40727bee"
+};
 
-  if(!firebase.apps.length){
-    firebae.intializeApp(firebaseConfig)
-  }
+// Initialize Firebase only once
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
