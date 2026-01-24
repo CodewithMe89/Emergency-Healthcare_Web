638f8e28d670dce6352329edf5c2ac506d424d3a
Keshav <keshav9899828464@gmail.com>
2026-01-24 12:30:37 +0530

    Create firebase.js

---
+++ JavaScript/firebase.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAwl22tT7kJD1U0toxE0WckzOxHAao9Nzg",
+    authDomain: "emergency-healthcare-17c29.firebaseapp.com",
+    projectId: "emergency-healthcare-17c29",
+    storageBucket: "emergency-healthcare-17c29.firebasestorage.app",
+    messagingSenderId: "210921508011",
+    appId: "1:210921508011:web:8308e17f37863a40727bee"
+  };
+
+  if(!firebase.apps.length){
+    firebae.intializeApp(firebaseConfig)
+  }
