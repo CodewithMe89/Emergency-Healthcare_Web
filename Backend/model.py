450a0476b65d76399aebaffbfa361050ad3f7dd8
Keshav <keshav9899828464@gmail.com>
2026-03-15 14:30:53 +0530

    Create model.py

---
+++ Backend/model.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+model = tf.keras.models.load_model("model.h5")
+
+def detect_accident(img):
+
+    img = img.resize((224,224))
+    img = np.array(img)/255.0
+    img = np.expand_dims(img, axis=0)
+
+    pred = model.predict(img)[0][0]
+
+    if pred > 0.6:
+        return "ACCIDENT"
+    else:
+        return "NO_ACCIDENT"
