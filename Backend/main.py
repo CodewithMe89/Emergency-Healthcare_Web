0012b2354114b7ff7aecb468609aa30807262aac
Keshav <keshav9899828464@gmail.com>
2026-03-15 14:30:49 +0530

    Create main.py

---
+++ Backend/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+from PIL import Image
+import io
+from model import detect_accident
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+@app.post("/detect-accident")
+async def detect_accident_api(file: UploadFile = File(...)):
+
+    contents = await file.read()
+
+    image = Image.open(io.BytesIO(contents))
+
+    result = detect_accident(image)
+
+    return {"result": result}
