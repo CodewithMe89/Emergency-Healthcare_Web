2fec62e68b8cc9933cc7e10728c56dfe3dc62f11
Keshav <keshav9899828464@gmail.com>
2026-04-03 12:49:44 +0530

    Update hospital_find.js

--- JavaScript/hospital_find.js
+++ JavaScript/hospital_find.js
@@ -75,7 +75,11 @@ try {
         }
       );
 
+      if(!response.ok){
+        throw new Error(`HTTP error: ${response.status}`)
+      }
       const data = await response.json();
+      console.log(data)
 
       list.innerHTML = "";
 
