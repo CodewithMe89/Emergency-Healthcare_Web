9312a2ceeea1b3e234fa0ffeeea4e9f45aaae7d2
Keshav <keshav9899828464@gmail.com>
2026-04-12 14:44:54 +0530

    Revert "update completeprofile"
    This reverts commit 511154ab99533d880bc271b8e9ba85a2ed5ea04b.

--- JavaScript/complete_profile.js
+++ JavaScript/complete_profile.js
@@ -1,7 +1,15 @@
+const firebaseApp = !firebase.apps || firebase.apps.length === 0
+  ? firebase.initializeApp({
+    apiKey: "AIzaSyAwl22tT7kJD1U0toxE0WckzOxHAao9Nzg",
+    authDomain: "emergency-healthcare-17c29.firebaseapp.com",
+    projectId: "emergency-healthcare-17c29",
+    storageBucket: "emergency-healthcare-17c29.firebasestorage.app",
+    messagingSenderId: "210921508011",
+    appId: "1:210921508011:web:8308e17f37863a40727bee"
+  })
+  : firebase.app();
 const auth = firebase.auth();
 const db = firebase.firestore();
-
-document.body.style.display = "none";
 
 auth.onAuthStateChanged(async (user) => {
   if (!user) {
@@ -9,14 +17,13 @@ auth.onAuthStateChanged(async (user) => {
     return;
   }
 
-  const snap = await db.collection('users').doc(user.uid).get();
+  const userRef = db.collection("users").doc(user.uid);
+  const snap = await userRef.get();
 
   // If already completed → block access
 
   if (snap.exists && snap.data().profileComplete === true) {
     window.location.href = "index.html";
-  } else {
-    document.body.style.display = "block";
   }
 });
 
@@ -27,37 +34,20 @@ document
 
     const contact = document.getElementById("contact").value.trim();
     const emergencyContact = document.getElementById("emergencyContact").value.trim();
-    const medical = document.getElementById('medical').value.trim();
-    const allergies = document.getElementById('allergies').value.trim()
 
     if (!contact || !emergencyContact) {
-      alert("Phone and Emergency Contact are required");
+      alert("All fields are required");
       return;
     }
-    const phoneRegex = /^[0-9]{10}$/;
 
-    if (!phoneRegex.test(contact) || !phoneRegex.test(emergencyContact)) {
-      alert("Enter valid 10-digit numbers");
-      return;
-    }
     const user = auth.currentUser;
-    if (!user) {
-      alert("Session expired. Please log in again.")
-      window.location.href = "Login.html"
-      return;
-    }
-    try {
-      await db.collection("users").doc(user.uid).set({
-        contact,
-        emergencyContact,
-        medicalHistory: medical,
-        allergies: allergies,
-        profileComplete: true
-      }, { merge: true });
-      alert("Profile updated successfully!")
-      window.location.href = "index.html";
-    } catch (err) {
-      alert("update failed: " + err.message)
-    }
+    if (!user) return;
 
+    await db.collection("users").doc(user.uid).set({
+      contact,
+      emergencyContact,
+      profileComplete: true
+    }, {merge: true});
+
+    window.location.href = "index.html";
   });
